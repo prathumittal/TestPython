fa722b06de5a13c47e7b5cbc0340cbffc05ccb7d
Puneet Mittal <puneetmittal@outlook.com>
2025-11-14 18:06:24 +0530

    Adding code

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -5,11 +5,11 @@ if num < 2:
     print(f"{num} is not a prime number")
 else:
     is_prime = True
-    for i in range(2, int(num ** 0.5) + 1):
+    for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             is_prime = False
             break
-    
+
     if is_prime:
         print(f"{num} is a prime number")
     else:
